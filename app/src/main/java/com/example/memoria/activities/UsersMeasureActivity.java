0ff392c06ca9a471e880ad8fd535950f3ac7b7e0
catalina <catalina.gonzalez.i@gmail.com>
2020-11-11 05:16:44 -0300

    get data of all editTexts in recycleview and send it to new activity

--- app/src/main/java/com/example/memoria/activities/UsersMeasureActivity.java
+++ app/src/main/java/com/example/memoria/activities/UsersMeasureActivity.java
@@ -1,10 +1,15 @@
 package com.example.memoria.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -18,14 +23,16 @@ import java.util.List;
 public class UsersMeasureActivity extends AppCompatActivity {
     private RecyclerView mRecyclerView;
     private List<String> names;
-    private RecyclerView.Adapter mAdapter;
+    public RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
+    private Button buttonFirstAnalysis;
 
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_users_measure);
+
         Bundle bundle = getIntent().getExtras();
         if (bundle != null){
             String users = bundle.getString("quantity", "0");
@@ -35,18 +42,32 @@ public class UsersMeasureActivity extends AppCompatActivity {
             mRecyclerView = (RecyclerView) findViewById(R.id.recyclerViewNames);
             mLayoutManager = new LinearLayoutManager(this);
             //Aquí digo que vista quiero adaptar
-            mAdapter = new SecondPageAdapter(names, R.layout.item_page2_analysis1, new SecondPageAdapter.OnItemClickListener() {
-                @Override
-                public void onItemClick(String name, int position) {
-                    Toast.makeText(UsersMeasureActivity.this,name + "-"+ position, Toast.LENGTH_LONG).show();
-                }
-            });
+            mAdapter = new SecondPageAdapter(names);//, R.layout.item_page2_analysis1
 
+            mRecyclerView.setItemAnimator(new DefaultItemAnimator());
             mRecyclerView.setLayoutManager(mLayoutManager);
             mRecyclerView.setAdapter(mAdapter);
+            buttonFirstAnalysis = (Button) findViewById(R.id.buttonGoAnalysis);
+
+
+            buttonFirstAnalysis.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(UsersMeasureActivity.this, "Funciona", Toast.LENGTH_LONG).show();
+                    String[] escrito = ((SecondPageAdapter)mAdapter).getEscrito();
+                    Intent goPage3 = new Intent(UsersMeasureActivity.this, FirstAnalysisActivity.class);
+                    goPage3.putExtra("people", escrito);
+                    startActivity(goPage3);
+
+                }
+            });
         } else{
             Toast.makeText(UsersMeasureActivity.this, "Por favor vuelva a intentarlo", Toast.LENGTH_LONG).show();
         }
+
+
+
+
 
 
     }
--- app/src/main/java/com/example/memoria/adapters/SecondPageAdapter.java
+++ app/src/main/java/com/example/memoria/adapters/SecondPageAdapter.java
@@ -1,42 +1,52 @@
 package com.example.memoria.adapters;
 
+import android.graphics.Color;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.memoria.R;
+import com.example.memoria.activities.UsersMeasureActivity;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SecondPageAdapter extends RecyclerView.Adapter<SecondPageAdapter.ViewHolder> {
 
     private List<String> names;
     private int layout;
-    private OnItemClickListener itemClickListener;
+    private String[] escrito;
+    //private OnItemClickListener itemClickListener;
 
-    public SecondPageAdapter(List<String> names, int layout, OnItemClickListener listener) {
+    public SecondPageAdapter(List<String> names) { //, OnItemClickListener listener
         this.names = names;
         this.layout = layout;
-        this.itemClickListener = listener;
+        this.escrito = new String[names.size()];
+        //this.itemClickListener = listener;
     }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(parent.getContext()).inflate(layout, parent, false);
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_page2_analysis1, parent, false);
         ViewHolder vh = new ViewHolder(v);
         return vh;
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        holder.bind(names.get(position), itemClickListener);
+        holder.bind(names.get(position));
     }
 
     @Override
@@ -44,13 +54,17 @@ public class SecondPageAdapter extends RecyclerView.Adapter<SecondPageAdapter.Vi
         return names.size();
     }
 
+    public String[] getEscrito() {
+        return escrito;
+    }
 
     //ViewHolder class
-    public static class ViewHolder extends RecyclerView.ViewHolder{
+    public class ViewHolder extends RecyclerView.ViewHolder{
         public TextView textViewUsers;
         public EditText editTextUserName;
         public TextView textViewInstruction;
         public Button buttonMeasure;
+        public ImageView loading;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -58,23 +72,54 @@ public class SecondPageAdapter extends RecyclerView.Adapter<SecondPageAdapter.Vi
             this.editTextUserName = (EditText) itemView.findViewById(R.id.analysis1EditPersonName);
             this.textViewInstruction = (TextView) itemView.findViewById(R.id.textViewInstructionMeasure);
             this.buttonMeasure = (Button) itemView.findViewById(R.id.buttonMeasure);
+            this.loading = (ImageView) itemView.findViewById(R.id.imageWaiting);
+            editTextUserName.addTextChangedListener(new TextWatcher() {
+                @Override
+                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                }
+
+                @Override
+                public void onTextChanged(CharSequence s, int start, int before, int count) {
+                    escrito[getAdapterPosition()] = s.toString();
+                }
+
+                @Override
+                public void afterTextChanged(Editable s) {
+
+                }
+            });
         }
 
-        public void bind(final String UserNumber, final OnItemClickListener listener){
+        public void bind(final String UserNumber){
             this.textViewUsers.setText(UserNumber);
-            this.textViewInstruction.setText(" Para medir debe ponerse en la posición donde estará este usuario conectado y oprimir el botón 'medir'");
-            /*itemView.setOnClickListener(new View.OnClickListener() {
+            this.textViewInstruction.setText(" Para medir debe ponerse en la ubicación donde estará este usuario conectado y oprimir el botón 'medir'");
+
+            final List<String> namesUsers =new ArrayList<>();
+            buttonMeasure.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    listener.onItemClick(UserNumber, getAdapterPosition());
+                    String name = editTextUserName.getText().toString();
+                    if (TextUtils.isEmpty(name)){
+                        Toast.makeText(itemView.getContext(), "Por favor ingrese un nombre", Toast.LENGTH_SHORT).show();
+                        return;
+                    } else {
+                        loading.setImageResource(R.drawable.waiting);
+                        buttonMeasure.setEnabled(false);
+                        namesUsers.add(name);
+
+                        loading.setImageResource(R.drawable.done);
+                    }
+
+
                 }
-            });*/
+            });
         }
     }
 
 
     //Function for click in textView
-    public interface OnItemClickListener{
-        void onItemClick(String name, int position);
+    public interface holita{
+        void getEscrito();
     }
 }
